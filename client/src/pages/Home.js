fcece90f2f7670531d82d2e8496eadcfc2a0a3f5
Kenny Joseph <kjoseph@bitpay.com>
2022-08-26 11:21:33 -0400

    A little better home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,13 @@ import '../styles/home.scss';
 
 export const Home = (props) => {
   return (
-    <div>Hello World!</div>
+    <div id='home-page' class='flex-column justify-content-center flex-grow'>
+      <div class='flex-row justify-content-center'>
+        <div class='flex-column'>
+          <h1 style='font-weight:normal;margin:0;'>Welcome to <span style='font-family: Ubuntu; font-weight:bold;'>bitdrip</span></h1>
+          <h2 style='font-weight:normal;margin:0;'>a faucet by <span style='font-family: Ubuntu; font-weight:bold;'>bitpay</span></h2>
+        </div>
+      </div>
+    </div>
   )
 }
